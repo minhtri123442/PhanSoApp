db94473d9701191f4ef0a1c078b1d1323eb8aeb3
Chau Tran Minh Tri <minhtrichautran@gmail.com>
2026-06-11 12:55:45 +0700

    Commit assignment

---
+++ PhanSoApp/PhanSo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanSoApp
+{
+    public class PhanSo
+    {
+        public int TuSo { get; private set; }
+        public int MauSo { get; private set; }
+
+        //constructor default
+
+        public PhanSo(int tuso, int mauso)
+        {
+            if (mauso == 0)
+            {
+                throw new ArgumentException(" Mau so ko duoc bang 0");
+                
+            }
+            if (mauso < 0)
+            {
+                tuso = -tuso;
+                mauso = -mauso;
+            }
+            int ucln = UCLN(Math.Abs(tuso), mauso);
+            TuSo = tuso / ucln;
+            MauSo = mauso / ucln;
+        }
+
+        private static int UCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        public PhanSo RutGon()
+        {
+            return new PhanSo(TuSo, MauSo);// constructor da tu dong rut
+        }
+
+        public override string ToString()
+        {
+            if (MauSo == 1) return TuSo.ToString();
+            return $"{TuSo}/{MauSo}";
+        }
+
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            int tuSoMoi = (a.TuSo * b.MauSo) + (a.MauSo * b.TuSo);
+            int mauSoMoi = a.MauSo * b.MauSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+        public static PhanSo operator -(PhanSo a, PhanSo b)
+        {
+            int tuSoMoi = a.TuSo * b.MauSo - a.MauSo * b.TuSo;
+            int mausoMoi = a.MauSo * b.MauSo;
+            return new PhanSo(tuSoMoi, mausoMoi);
+        }
+
+        public static PhanSo operator * (PhanSo a, PhanSo b)
+        {
+            int tuSoMoi = a.TuSo * b.TuSo;
+            int mauSoMoi = a.MauSo * b.MauSo;
+            return new PhanSo(tuSoMoi, mauSoMoi);
+        }
+
+
+        public static PhanSo operator / (PhanSo a, PhanSo b)
+        {
+            if(b.TuSo ==0)
+            {
+                throw new DivideByZeroException("khong the chia phan so cho 0");
+            }
+            return new PhanSo(a.TuSo * b.MauSo, a.MauSo * b.TuSo);
+
+        }
+
+
+        //nang cao
+        public static PhanSo operator + (PhanSo a , int b)
+        {
+            PhanSo n = new PhanSo(b, 1);
+            return a + n;
+        }
+        public static PhanSo operator +(int a , PhanSo b)
+        {
+            return b + a;
+        }
+
+
+        // bai 3
+        public static bool operator ==(PhanSo a, PhanSo b)
+        {
+            return a.TuSo * b.MauSo == a.MauSo * b.TuSo;
+        }
+
+        public static bool operator !=(PhanSo a, PhanSo b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator <(PhanSo a, PhanSo b)
+        {
+            return a.TuSo * b.MauSo < a.MauSo * b.TuSo;
+        }
+
+        public static bool operator >(PhanSo a, PhanSo b)
+        {
+            return b < a;
+        }
+
+    }
+}
---
+++ PhanSoApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using PhanSoApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PhanSoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PhanSo ps1 = new PhanSo(1, 2);  // 1/2
+            //PhanSo ps2 = new PhanSo(1, 3);  // 1/3
+
+            //Console.WriteLine($"ps1 = {ps1}");
+            //Console.WriteLine($"ps2 = {ps2}");
+            //Console.WriteLine($"ps1 + ps2 = {ps1 + ps2}");  // Ket qua: 5/6
+            //Console.WriteLine($"ps1 - ps2 = {ps1 - ps2}");  // Ket qua: 1/6
+            //Console.WriteLine($"ps1 * ps2 = {ps1 * ps2}");  // Ket qua: 1/6
+            //Console.WriteLine($"ps1 / ps2 = {ps1 / ps2}");  // Ket qua: 3/2
+
+
+
+            //yeu cau nang cao
+            //PhanSo ps1 = new PhanSo(1, 2); // 1/2
+            //int n = 2;
+
+            //PhanSo ketQua1 = ps1 + n; // Kết quả: 5/2
+            //PhanSo ketQua2 = n + ps1; // Kết quả: 5/2
+
+            //Console.WriteLine($"{ps1} + {n} = {ketQua1}");
+            //Console.WriteLine($"{n} + {ps1} = {ketQua2}");
+
+
+            //bai tap 3
+            PhanSo ps1 = new PhanSo(1, 2);  // 1/2
+            PhanSo ps2 = new PhanSo(2, 4);  // 2/4 (= 1/2 sau khi rut gon)
+            PhanSo ps3 = new PhanSo(1, 3);  // 1/3
+
+            Console.WriteLine(ps1 == ps2);  // True  (1/2 == 2/4)
+            Console.WriteLine(ps1 != ps3);  // True  (1/2 != 1/3)
+            Console.WriteLine(ps3 < ps1);   // True  (1/3 < 1/2)
+            Console.WriteLine(ps1 > ps3);
+
+
+        }
+    }
+}
